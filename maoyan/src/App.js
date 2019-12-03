22ee0b0fe4c83317ecbb6a5e3e2034a85505503f
Rongzhen666 <38045439+Rongzhen666@users.noreply.github.com>
2019-12-03 11:33:32 +0800

    在GitHub网站直接修改12-03

--- maoyan/src/App.js
+++ maoyan/src/App.js
@@ -5,18 +5,8 @@ import  {
     Switch
 } from "react-router-dom"
 import  Home from "./views/Home"
-<<<<<<< HEAD
-import  Detail from "./views/detail"
 import Login from "./views/Login";
 import Register from "./views/Register";
-function App() {
-  return (
-    <div className="App">
-            <Route path={"/nogin"} component={Login}></Route>
-        <Route path={"/reg"} component={Register}></Route>
-        <Route path={"/"} component={Home}></Route>
-        <Route path={"/detail"} component={Detail}></Route>
-=======
 import  DetailMovie from "./views/DetailMovie"
 import  DetailCinema from "./views/DetailCinema"
 import  InformationMovie from "./views/InformationMovie"
@@ -28,10 +18,12 @@ function App() {
   return (
     <div className="App">
         <Switch>
- <Route path={"/cinemaList"} component={CinemaList}></Route>
-            <Route path={"/moviesList"} component={MoviesList}></Route>
-            <Route path={"/position"} component={Position}></Route>
-            <Route path={"/search"} component={Search}></Route>
+          <Route path={"/cinemaList"} component={CinemaList}></Route>
+          <Route path={"/nogin"} component={Login}></Route>
+          <Route path={"/reg"} component={Register}></Route>
+          <Route path={"/moviesList"} component={MoviesList}></Route>
+          <Route path={"/position"} component={Position}></Route>
+          <Route path={"/search"} component={Search}></Route>
           <Route path={"/informationMovie"} component={InformationMovie}></Route>
           <Route path={"/detailCinema"} component={DetailCinema}></Route>
           <Route path={"/detailMovie"} component={DetailMovie}></Route>
@@ -39,9 +31,9 @@ function App() {
         </Switch>
         
         
->>>>>>> 127dbd52298ba5fbe4ddc82cd169c34e0abbe3b2
     </div>
   );
 }
 
 export default App;
+
